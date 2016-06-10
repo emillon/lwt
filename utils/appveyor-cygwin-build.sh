ca975552b6d284d8570f2d274a809b933c52202d
Anton Bachin <antonbachin@yahoo.com>
2016-06-10 08:08:20 -0500

    AppVeyor: cache OPAM dependencies

--- utils/appveyor-cygwin-build.sh
+++ utils/appveyor-cygwin-build.sh
@@ -3,5 +3,15 @@ set -x
 
 ocamlc -version
 
-opam pin add -y --no-action .
+if [ -d ../opam-cache-$COMPILER ]
+then
+    cp -r ../opam-cache-$COMPILER ~/.opam
+    eval `opam config env`
+else
+    opam pin add -y --no-action .
+    opam install -y --deps-only lwt
+
+    cp -r ~/.opam ../opam-cache-$COMPILER
+fi
+
 opam install -y --build-test --keep-build-dir lwt
--- utils/appveyor-cygwin-install.sh
+++ utils/appveyor-cygwin-install.sh
@@ -1,13 +1,16 @@
 set -e
 set -x
 
-opam init -y --auto-setup
+if [ ! -d ../opam-cache-$COMPILER ]
+then
+    opam init -y --auto-setup
 
-cd ..
-git clone https://github.com/ocaml/oasis.git
-cd oasis
-git checkout 0.4.6
-wget -O - https://github.com/Chris00/oasis/commit/4316cf28797ab0686196e0d90651ebf0cdfb6319.patch | git am
-wget -O - https://github.com/pwbs/oasis/commit/00640c1ef3ea4a3790699c0e35a15cad70a7a4b4.patch | git am
-opam pin add -y --no-action oasis .
-cd ../lwt
+    cd ..
+    git clone https://github.com/ocaml/oasis.git
+    cd oasis
+    git checkout 0.4.6
+    wget -O - https://github.com/Chris00/oasis/commit/4316cf28797ab0686196e0d90651ebf0cdfb6319.patch | git am
+    wget -O - https://github.com/pwbs/oasis/commit/00640c1ef3ea4a3790699c0e35a15cad70a7a4b4.patch | git am
+    opam pin add -y --no-action oasis .
+    cd ../lwt
+fi
